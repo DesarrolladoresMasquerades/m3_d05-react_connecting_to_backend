b24d53e8d915289bb37f223edec42b8868c645f1
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-04 13:18:41 +0100

    done lesson

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import ApartmentsPage from "./pages/ApartmentsPage";
-import AddApartmentPage from "./pages/AddApartmentPage";
+import ApartmentDetailPage from "./pages/ApartmentDetailPage";
+import ApartmentEditPage from "./pages/ApartmentEditPage";
+//import AddApartmentPage from "./pages/AddApartmentPage";
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<ApartmentsPage />} />
+        <Route exact path="/apartments/:apartmentId/edit" element={<ApartmentEditPage />} />
+        <Route exact path="/apartments/:apartmentId" element={<ApartmentDetailPage />} />
+        <Route exact path="/" element={<ApartmentsPage />} />
       </Routes>
     </div>
   );
