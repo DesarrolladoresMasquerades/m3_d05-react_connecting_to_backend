8ac33bec9301688a2a9191c735bb0d871aba1198
marcosantonastasi <marco.santonastasi@gmail.com>
2022-03-04 10:01:27 +0100

    starter code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<ApartmentsPage />} />
-        <Route path="/apartments/new" element={<AddApartmentPage />} />
       </Routes>
     </div>
   );
